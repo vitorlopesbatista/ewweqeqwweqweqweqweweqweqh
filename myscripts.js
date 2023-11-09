ab06103fb693d28988a1bd91ae86f4d716ce3865
vitorlopesbatista <129400838+vitorlopesbatista@users.noreply.github.com>
2023-11-09 11:21:38 -0300

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,8 @@
 function calcularMCU() {
     // Obtenha os valores de entrada do usuário
-    const raio = parseFloat(document.getElementById("raio").value);
-    const velocidadeAngular = parseFloat(document.getElementById("velocidadeAngular").value);
-    const tempo = parseFloat(document.getElementById("tempo").value);
+    const raio = parseFloat(document.querySelector("raio").value);
+    const velocidadeAngular = parseFloat(document.querySelector("velocidadeAngular").value);
+    const tempo = parseFloat(document.querySelector("tempo").value);
 
     // Calcule o ângulo em radianos
     const theta = velocidadeAngular * tempo;
@@ -13,7 +13,7 @@ function calcularMCU() {
     const aceleracao = raio * Math.pow(velocidadeAngular, 2);
 
     // Exiba os resultados na página
-    document.getElementById("resultadoPosicao").innerHTML = "Posição: " + posicao.toFixed(2);
-    document.getElementById("resultadoVelocidade").innerHTML = "Velocidade: " + velocidade.toFixed(2);
-    document.getElementById("resultadoAceleracao").innerHTML = "Aceleração: " + aceleracao.toFixed(2);
+    document.querySelector("resultadoPosicao").innerHTML = "Posição: " + posicao.toFixed(2);
+    document.querySelector("resultadoVelocidade").innerHTML = "Velocidade: " + velocidade.toFixed(2);
+    document.querySelector("resultadoAceleracao").innerHTML = "Aceleração: " + aceleracao.toFixed(2);
 }
